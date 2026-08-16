8169621b7b668a90a5cd0c486fb5098abf51497b
Brandon Potts <bepotts47@gmail.com>
2026-08-16 16:02:18 -0400

    Initial Commit

---
+++ TimeOnTaskTests/TimeOnTaskTests.swift
@@ -0,0 +1,19 @@
+//
+//  TimeOnTaskTests.swift
+//  TimeOnTaskTests
+//
+//  Created by Brandon Potts on 8/16/26.
+//
+
+import Testing
+@testable import TimeOnTask
+
+struct TimeOnTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
